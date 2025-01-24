2b1225ad164f916ddadc548b0b8c4bf19ea8cb3b
CASPER <CASPER@DESKTOP-NVI4GOK>
2025-01-24 20:28:42 +0300

    Proje dosyası ekle.

---
+++ Proje1/Form3.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace Proje1
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            panel1 = new Panel();
+            comboBoxmodel = new ComboBox();
+            combopictureBox = new PictureBox();
+            button1 = new Button();
+            panel9 = new Panel();
+            pictureBox6 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            panel8 = new Panel();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            serit9 = new Panel();
+            serit8 = new Panel();
+            serit7 = new Panel();
+            serit6 = new Panel();
+            serit3 = new Panel();
+            serit5 = new Panel();
+            serit2 = new Panel();
+            serit4 = new Panel();
+            serit1 = new Panel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            errorProvider1 = new ErrorProvider(components);
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)combopictureBox).BeginInit();
+            panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(comboBoxmodel);
+            panel1.Controls.Add(combopictureBox);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(panel9);
+            panel1.Controls.Add(panel8);
+            panel1.Controls.Add(serit9);
+            panel1.Controls.Add(serit8);
+            panel1.Controls.Add(serit7);
+            panel1.Controls.Add(serit6);
+            panel1.Controls.Add(serit3);
+            panel1.Controls.Add(serit5);
+            panel1.Controls.Add(serit2);
+            panel1.Controls.Add(serit4);
+            panel1.Controls.Add(serit1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(629, 486);
+            panel1.TabIndex = 0;
+            // 
+            // comboBoxmodel
+            // 
+            comboBoxmodel.BackColor = Color.Silver;
+            comboBoxmodel.FormattingEnabled = true;
+            comboBoxmodel.Location = new Point(167, 41);
+            comboBoxmodel.Name = "comboBoxmodel";
+            comboBoxmodel.Size = new Size(289, 28);
+            comboBoxmodel.TabIndex = 14;
+            comboBoxmodel.SelectedIndexChanged += comboBoxmodel_SelectedIndexChanged;
+            // 
+            // combopictureBox
+            // 
+            combopictureBox.Location = new Point(246, 114);
+            combopictureBox.Name = "combopictureBox";
+            combopictureBox.Size = new Size(125, 177);
+            combopictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            combopictureBox.TabIndex = 14;
+            combopictureBox.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Navy;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Verdana", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.ForeColor = SystemColors.Control;
+            button1.Location = new Point(215, 348);
+            button1.Name = "button1";
+            button1.Size = new Size(214, 96);
+            button1.TabIndex = 13;
+            button1.Text = "OYNA";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel9
+            // 
+            panel9.BackColor = Color.Lime;
+            panel9.Controls.Add(pictureBox6);
+            panel9.Controls.Add(pictureBox4);
+            panel9.Controls.Add(pictureBox5);
+            panel9.Dock = DockStyle.Left;
+            panel9.ForeColor = SystemColors.ControlDark;
+            panel9.Location = new Point(0, 0);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(77, 486);
+            panel9.TabIndex = 1;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(12, 412);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(55, 62);
+            pictureBox6.TabIndex = 16;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(12, -19);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(55, 62);
+            pictureBox4.TabIndex = 14;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(12, 176);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(55, 62);
+            pictureBox5.TabIndex = 15;
+            pictureBox5.TabStop = false;
+            // 
+            // panel8
+            // 
+            panel8.BackColor = Color.Lime;
+            panel8.Controls.Add(pictureBox1);
+            panel8.Controls.Add(pictureBox3);
+            panel8.Controls.Add(pictureBox2);
+            panel8.Dock = DockStyle.Right;
+            panel8.ForeColor = SystemColors.ControlDark;
+            panel8.Location = new Point(552, 0);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(77, 486);
+            panel8.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(10, 412);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(55, 62);
+            pictureBox1.TabIndex = 19;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(10, -19);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(55, 62);
+            pictureBox3.TabIndex = 17;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(10, 176);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(55, 62);
+            pictureBox2.TabIndex = 18;
+            pictureBox2.TabStop = false;
+            // 
+            // serit9
+            // 
+            serit9.BackColor = Color.White;
+            serit9.Location = new Point(447, 431);
+            serit9.Name = "serit9";
+            serit9.Size = new Size(13, 86);
+            serit9.TabIndex = 12;
+            // 
+            // serit8
+            // 
+            serit8.BackColor = Color.White;
+            serit8.Location = new Point(447, 245);
+            serit8.Name = "serit8";
+            serit8.Size = new Size(13, 86);
+            serit8.TabIndex = 11;
+            // 
+            // serit7
+            // 
+            serit7.BackColor = Color.White;
+            serit7.Location = new Point(447, 41);
+            serit7.Name = "serit7";
+            serit7.Size = new Size(13, 86);
+            serit7.TabIndex = 10;
+            // 
+            // serit6
+            // 
+            serit6.BackColor = Color.White;
+            serit6.Location = new Point(305, 431);
+            serit6.Name = "serit6";
+            serit6.Size = new Size(13, 86);
+            serit6.TabIndex = 9;
+            // 
+            // serit3
+            // 
+            serit3.BackColor = Color.White;
+            serit3.Location = new Point(167, 431);
+            serit3.Name = "serit3";
+            serit3.Size = new Size(13, 86);
+            serit3.TabIndex = 9;
+            // 
+            // serit5
+            // 
+            serit5.BackColor = Color.White;
+            serit5.Location = new Point(305, 245);
+            serit5.Name = "serit5";
+            serit5.Size = new Size(13, 86);
+            serit5.TabIndex = 8;
+            // 
+            // serit2
+            // 
+            serit2.BackColor = Color.White;
+            serit2.Location = new Point(167, 245);
+            serit2.Name = "serit2";
+            serit2.Size = new Size(13, 86);
+            serit2.TabIndex = 8;
+            // 
+            // serit4
+            // 
+            serit4.BackColor = Color.White;
+            serit4.Location = new Point(305, 41);
+            serit4.Name = "serit4";
+            serit4.Size = new Size(13, 86);
+            serit4.TabIndex = 7;
+            // 
+            // serit1
+            // 
+            serit1.BackColor = Color.White;
+            serit1.Location = new Point(167, 41);
+            serit1.Name = "serit1";
+            serit1.Size = new Size(13, 86);
+            serit1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            timer1.Tick += timer1_Tick;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(629, 486);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "CAR GAME";
+            Load += Form3_Load;
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)combopictureBox).EndInit();
+            panel9.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel9;
+        private Panel panel8;
+        private Panel serit9;
+        private Panel serit8;
+        private Panel serit7;
+        private Panel serit6;
+        private Panel serit3;
+        private Panel serit5;
+        private Panel serit2;
+        private Panel serit4;
+        private Panel serit1;
+        private Button button1;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private ComboBox comboBoxmodel;
+        private PictureBox combopictureBox;
+        private ErrorProvider errorProvider1;
+    }
+}
